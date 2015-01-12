d05b4a6165ea5069fb811a7715f5b8461f376ec7
Enver Bisevac <enver@Envers-MacBook-Pro.local>
2015-01-12 10:12:39 +0100

    dingo support for 1.7

--- ckeditor_filer/__init__.py
+++ ckeditor_filer/__init__.py
@@ -1,3 +1,7 @@
-from django.template.loader import add_to_builtins
+try:
+    from django.template.loader import add_to_builtins
+except:
+    from django.template.base import add_to_builtins
+
 
 add_to_builtins('ckeditor_filer.templatetags.ckeditor_tags')
